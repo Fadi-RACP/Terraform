22245330edf3731508b0cf4d141c6759c9a8cb39
Fadi-RACP <Admin_FAlafen@racp.edu.au>
2023-06-26 11:02:47 +1000

    updated secret

--- provider.tf
+++ provider.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
   subscription_id = "f107218a-dbce-4907-ae09-f7916aa43d80"
   tenant_id = "09c2d83f-ca57-4dad-8a0b-502b18e773e8"
   client_id = "aa696f96-e7b9-4814-9033-3785a2320c60"
-  client_secret = "sBd8Q~Fk48s.Kc-z.lwmPoLFBEiOZZiX2gWnAbpY"
+  client_secret = "u648Q~HeeHfAUa1tlPQtjlVkpd9.ZweieqVZ2dvj"
   features {}
 }
 
