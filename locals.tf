14eb8bc3983dc42861f0b10a543535b5dca80b36
Fadi-RACP <135833173+Fadi-RACP@users.noreply.github.com>
2023-07-06 10:17:13 +1000

    Update locals.tf
    added the VNet name in locals

--- locals.tf
+++ locals.tf
@@ -3,7 +3,8 @@ locals {
   resource_group_name = "Test-grp"
   location = "Australia East"
   virtual_network = {
-    address_space = "10.45.0.0/20"
+   Name           = "Test-VNet"  
+   address_space  = "10.45.0.0/20"
   }
 }
 
