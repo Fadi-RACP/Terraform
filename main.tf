cf80d697217837d88167b5cdd478272a936d5239
Fadi-RACP <Admin_FAlafen@racp.edu.au>
2023-07-06 10:43:19 +1000

    testing modules in terraform

--- main.tf
+++ main.tf
@@ -7,27 +7,8 @@ resource "azurerm_resource_group" "Test-grp" {
   }
 }
 
-resource "azurerm_virtual_network" "Test-VNet" {
-  name                = "${terraform.workspace}-VNet"
-  location            = local.location 
-  resource_group_name = azurerm_resource_group.Test-grp.name
-  address_space       = [local.virtual_network.address_space]
-  depends_on = [
-    azurerm_resource_group.Test-grp
-  ]  
-  tags = {
-    "Environment" = "Test"
-  }
-} 
-
-resource "azurerm_subnet" "Test-Subnet" {    
-    name                 = "${terraform.workspace}-Subnet1"
-    resource_group_name  = azurerm_resource_group.Test-grp.name
-    virtual_network_name = azurerm_virtual_network.Test-VNet.name
-    address_prefixes     = [cidrsubnet(local.virtual_network.address_space,4,0)]
-    depends_on = [
-      azurerm_virtual_network.Test-VNet
-    ]
+module "Network" {
+  source = "./Network"
 }
 
 /*
