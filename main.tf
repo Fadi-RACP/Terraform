66c687a12522c8586e6edceae069c1dd66e1c4c6
Fadi-RACP <Admin_FAlafen@racp.edu.au>
2023-07-06 11:05:24 +1000

    fine tuning for the code

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "Test-grp" {
 resource "azurerm_virtual_network" "Test-VNet" {
   name                = "${terraform.workspace}-VNet"
   location            = local.location 
-  resource_group_name = azurerm_Resource_Group.Test-grp.name
+  resource_group_name = azurerm_resource_group.Test-grp.name
   address_space       = [local.virtual_network.address_space]
   depends_on = [
     azurerm_resource_group.Test-grp
@@ -22,7 +22,7 @@ resource "azurerm_virtual_network" "Test-VNet" {
 
 resource "azurerm_subnet" "Test-Subnet" {    
     name                 = "${terraform.workspace}-Subnet1"
-    resource_group_name  = azurerm_Resource_Group.Test-grp.name
+    resource_group_name  = azurerm_resource_group.Test-grp.name
     virtual_network_name = azurerm_virtual_network.Test-VNet.name
     address_prefixes     = [cidrsubnet(local.virtual_network.address_space,4,0)]
     depends_on = [
